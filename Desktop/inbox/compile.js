3710609822b3ab5338982bfc3c16fc05ca304764
Greg Osagie <greg_osagie@yahoo.com>
2022-08-30 17:46:24 +0100

    created tests for inbox

---
+++ Desktop/inbox/compile.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf-8');
+
+
+//module.export allows us to be able to require in our compile file
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
+
+
+
+
+
+
---
+++ Desktop/inbox/test/inbox.test.js
@@ -0,0 +1,45 @@
+const assert= require('assert');
+const ganache = require('ganache-cli');
+/* Web3 is a constructor function so it needs caps. it is used to create different instances, 
+the purpose of each instance is to connect to a different ethereum network */
+const Web3 = require('web3');
+//instance of web 3 
+const web3 = new Web3(ganache.provider());
+
+const {  interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+
+    //get a list of all accounts 
+    accounts = await web3.eth.getAccounts();
+
+    // use one of those accounts to deploy the contract
+
+   inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!']})
+    .send({from: accounts[0], gas: '1000000'})
+
+
+});
+describe('Inbox', () =>{
+    it('deploys a contract', () => {
+
+       assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message,'Hi there!');
+    
+    });
+
+    it('can change the message', async () => {
+
+        await inbox.methods.setMessage('I\'ve changed').send({from: accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.equal(message,'I\'ve changed');
+    });
+})
